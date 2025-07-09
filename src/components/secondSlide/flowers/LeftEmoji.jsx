b07c8064b4df48a2ca0002e126fa710e9440593e
Rishi Raj <rishi@Rishis-MacBook-Air.local>
2025-07-10 01:56:27 +0530

    emoji animation added

--- src/components/secondSlide/flowers/LeftEmoji.jsx
+++ src/components/secondSlide/flowers/LeftEmoji.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const LeftEmoji = () => {
   return (
-    <>
-      <div className=" main_circle border-4 size-56 rounded-full left-50 relative z-1 bg-white">
+    <div className="relative w-max h-max left-50 top-30">
+      <div className=" main_circle border-4 size-56 rounded-full relative z-10 bg-white">
         <div
           style={{ boxShadow: "inset -15px 2px 0px 2px rgba(0,0,0,0.2" }}
           className="main_circle_shadow rounded-full size-[13.5rem] absolute z-1"
@@ -13,14 +13,14 @@ const LeftEmoji = () => {
             className="eye_circle_shadow_left rounded-full size-[6.5rem] z-1 absolute "
           ></div>
           <div className="eye_circle_left bg-black rounded-full size-[6.5rem] absolute left-0 flex items-center justify-center ">
-            <div className="rounded-full border-4 border-white"></div>
+            <div className="animate-emoji-eyes-left rounded-full border-2 h-0 w-0 border-white"></div>
           </div>
           <div
             style={{ boxShadow: "inset -15px 5px 0px 2px rgba(50,48,51)" }}
             className="eye_circle_shadow_right rounded-full size-[6.5rem] absolute z-1 right-0"
           ></div>
           <div className="eye_circle_right rounded-full bg-black size-[6.5rem] absolute right-0  flex items-center justify-center">
-            <div className="rounded-full size-6 border-2 border-white"></div>
+            <div className="animate-emoji-eyes-right rounded-full size-6 border-2 border-white"></div>
           </div>
           <div className="smile_white_circle_left rounded-full size-20 bg-white absolute z-2 bottom-15 left-2 ">
             <div
@@ -39,13 +39,16 @@ const LeftEmoji = () => {
             className=" smile border-4 rounded-full absolute size-[9rem] left-8 top-8 z-3"
           ></div>
         </div>
-        <div className=" neck h-[2rem] w-[1.5rem] border-y-transparent border-4 absolute top-[100%] left-[45%]  bg-white"></div>
-        <div className="h-[3rem] w-[6rem] rounded-t-[2rem] border-4 absolute top-[112%] left-[30%] flex justify-between items-end bg-blue-500">
-          <div className="border-4 h-4 w-0 mx-3"></div>
-          <div className="border-4 h-4 w-0 mx-3"></div>
-        </div>
       </div>
-    </>
+      <div className="side_box">
+        <div className="shadow-emoji-box animate-emoji-box side_box_left h-[2.5rem] w-[4.5rem] absolute rounded-[30%]"></div>
+      </div>
+      <div className="h-[3rem] w-[6rem] rounded-t-[2rem] border-4 absolute top-[109%] z-1 left-[29%] flex justify-between items-end bg-blue-500">
+        <div className="border-4 h-4 w-0 mx-3"></div>
+        <div className="border-4 h-4 w-0 mx-3"></div>
+      </div>
+      <div className=" neck h-[2rem] w-[1.5rem] border-y-transparent border-4 absolute top-[97%] z-0 left-[45%]  bg-white"></div>
+    </div>
   );
 };
 
