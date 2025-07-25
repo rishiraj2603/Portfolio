3225cfbad1f1200bc18c0fec17c5cba1e4810193
Rishi Raj <rishi@Rishis-MacBook-Air.local>
2025-07-26 03:21:47 +0530

    project card 50% completed

--- src/components/thirdSlide/ThirdSlide.jsx
+++ src/components/thirdSlide/ThirdSlide.jsx
@@ -9,7 +9,7 @@ const ThirdSlide = () => {
       skills:
         "Node.js,Express,JWT,MongoDB,Socket.io,Axios,React,Material UI,RESTful APIs",
       description:
-        "Developed a full-stack Event Management Platform for creating, managing, and joining events •Implemented secure JWT-based user authentication with guest access and protected routes •Enabled event creation, deletion, and real-time attendee tracking with WebSocket integration •Built responsive UI using React.js with clean navigation and modern components •Integrated real-time updates for event modifications and live attendee counts",
+        "Developed a full-stack Event Management Platform for creating, managing, and joining events. •Implemented secure JWT-based user authentication with guest access and protected routes. •Enabled event creation, deletion, and real-time attendee tracking with WebSocket integration. •Built responsive UI using React.js with clean navigation and modern components. •Integrated real-time updates for event modifications and live attendee counts.",
       backGround_color: "#e0f3ff",
       buttonBackGround_color: "#c2ebfc",
     },
@@ -18,7 +18,7 @@ const ThirdSlide = () => {
       link: "https://test-harmoni.vercel.app/",
       skills: "React,Redux,Tailwind CSS,Axios,Responsive UI",
       description:
-        "Built a responsive e-commerce front-end using React, Redux, and Tailwind CSS •Integrated FakeStore API to fetch product, category, and detail data •Implemented search, category filtering, product detail modals, and cart indicator •Utilized Redux for global state management (cart and filters) •Designed reusable React components for modularity and maintainability •Ensured mobile-first responsive design with Tailwind CSS Utilities",
+        "Built a responsive e-commerce front-end using React, Redux, and Tailwind CSS. •Integrated FakeStore API to fetch product, category, and detail data. •Implemented search, category filtering, product detail modals, and cart indicator. •Utilized Redux for global state management (cart and filters). •Designed reusable React components for modularity and maintainability. •Ensured mobile-first responsive design with Tailwind CSS Utilities.",
       backGround_color: "#ffe1cb",
       buttonBackGround_color: "#ffd0b4",
     },
@@ -33,7 +33,7 @@ const ThirdSlide = () => {
     },
   ];
   return (
-    <div className="projects h-dvh w-dvw flex gap-10 justify-center items-center">
+    <div className="projects h-dvh w-dvw flex gap-10 justify-center items-center bg-[#d6ddea]">
       {projects.map((item) => {
         return <ProjectCard project={item}></ProjectCard>;
       })}
--- src/components/thirdSlide/projectCard/ProjectCard.jsx
+++ src/components/thirdSlide/projectCard/ProjectCard.jsx
@@ -1,36 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoMdArrowForward } from "react-icons/io";
 
 const ProjectCard = ({ project }) => {
   const { title, link, backGround_color, buttonBackGround_color } = project;
   const descriptionLines = project.description.split("•");
   const skill = project.skills.split(",");
-  console.log("🚀 ~ ProjectCard ~ skill:", skill);
+
+  const [flip, setFlip] = useState(true);
 
   return (
-    <div
-      className="projectCard h-[50%] w-[25%] rounded-4xl border-2 m-10"
-      style={{ backgroundColor: backGround_color }}
-    >
-      <div className="">
-        <div className="mx-auto my-auto border-2 w-fit">{title}</div>
-        <div>{descriptionLines[0]}</div>
-        <div className="">
-          {skill.map((skill) => {
-            return (
-              <button
-                style={{ backgroundColor: buttonBackGround_color }}
-                className="rounded-full h-[3rem] w-[7rem] border-2"
-              >
-                {skill}
-              </button>
-            );
+    <div className="projectCard shadow-projectCard opacity-80 bg-white h-[60%] w-[30%] rounded-4xl  m-10 p-2 transition duration-200 ease hover:shadow-projectCard-hover hover:opacity-100 hover:scale-100 scale-90">
+      {flip ? (
+        <div
+          className=" flex gap-4 p-4 h-[80%] w-[100%] flex-col rounded-4xl"
+          style={{ backgroundColor: backGround_color }}
+        >
+          <div className="text-4xl mt-2 w-fit">{title}</div>
+          <div className="my-5 mx-1">{descriptionLines[0]}</div>
+          <div className=" flex flex-wrap gap-2">
+            {skill.map((skill) => {
+              return (
+                <button
+                  style={{ backgroundColor: buttonBackGround_color }}
+                  className="rounded-full shadow-skillButton h-[3.5rem] w-[7.5rem] hover:shadow-projectCard-hover "
+                >
+                  {skill}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+      ) : (
+        <div className="m-2 p-2 text-xl border-2">
+          {descriptionLines.map((line) => {
+            return <p>• {line}</p>;
           })}
         </div>
-      </div>
-      <div className="hidden m-2">
-        {descriptionLines.map((line) => {
-          return <p>{line}</p>;
-        })}
+      )}
+      <div className="flex mx-3 p-6 justify-between items-center text-3xl">
+        {flip ? (
+          <p>Explore</p>
+        ) : (
+          <p>
+            Check Out the {}
+            <a href={link}>Link</a>
+          </p>
+        )}
+        <button
+          onClick={() => {
+            setFlip(!flip);
+          }}
+          className="flex bg-[#f6f6f6] rounded-xl justify-center items-center size-[3rem] "
+        >
+          <IoMdArrowForward />
+        </button>
       </div>
     </div>
   );
